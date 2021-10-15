1dd96f8caacbaf0e3e15dbe31239d69996172266
JonathanFrey0000 <82041401+JonathanFrey0000@users.noreply.github.com>
2021-10-15 03:16:44 -0700

    Update ApiUtil.java

--- src/main/java/com/crumbs/emailservice/util/ApiUtil.java
+++ src/main/java/com/crumbs/emailservice/util/ApiUtil.java
@@ -1,10 +1,11 @@
 package com.crumbs.emailservice.util;
-
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 
-@AllArgsConstructor
 public class ApiUtil{
+
+    private ApiUtil(){
+        throw new IllegalStateException("Utility class");
+    }
 
     @Getter private static final String CLIENT_URL = "https://crumbs-ss.link";
     @Getter private static final String AWS_SECRET_ACCESS_KEY = System.getenv("AWS_SECRET_ACCESS_KEY");
